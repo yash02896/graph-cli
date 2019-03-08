e1cef1343446fee6603e75e1307731ffafc9d352
Jannis Pohlmann <contact@jannispohlmann.de>
2019-03-08 16:10:11 +0100

    subgraph: Validate transaction functions in handlers

--- src/subgraph.js
+++ src/subgraph.js
@@ -180,47 +180,102 @@ Must be 40 hexadecimal characters, with an optional '0x' prefix.`,
       .reduce((errors, dataSource, dataSourceIndex) => {
         let path = ['dataSources', dataSourceIndex, 'eventHandlers']
 
+        let abi
         try {
           // Resolve the source ABI name into a real ABI object
           let abiName = dataSource.getIn(['source', 'abi'])
           let abiEntry = dataSource
             .getIn(['mapping', 'abis'])
             .find(abi => abi.get('name') === abiName)
-          let abi = ABI.load(abiEntry.get('name'), resolveFile(abiEntry.get('file')))
-
-          // Obtain event signatures from the mapping
-          let manifestEvents = dataSource
-            .getIn(['mapping', 'eventHandlers'])
-            .map(handler => handler.get('event'))
-
-          // Obtain event signatures from the ABI
-          let abiEvents = abi.eventSignatures()
-
-          // Add errors for every manifest event signature that is not
-          // present in the ABI
-          return manifestEvents.reduce(
-            (errors, manifestEvent, index) =>
-              abiEvents.includes(manifestEvent)
-                ? errors
-                : errors.push(
-                    immutable.fromJS({
-                      path: [...path, index],
-                      message: `\
-Event with signature '${manifestEvent}' not present in ABI '${abi.name}'.
-Available events:
-${abiEvents
-                        .sort()
-                        .map(event => `- ${event}`)
-                        .join('\n')}`,
-                    }),
-                  ),
-            errors,
-          )
+          abi = ABI.load(abiEntry.get('name'), resolveFile(abiEntry.get('file')))
         } catch (e) {
           // Ignore errors silently; we can't really say anything about
           // the events if the ABI can't even be loaded
           return errors
         }
+
+        // Obtain event signatures from the mapping
+        let manifestEvents = dataSource
+          .getIn(['mapping', 'eventHandlers'], immutable.List())
+          .map(handler => handler.get('event'))
+
+        // Obtain event signatures from the ABI
+        let abiEvents = abi.eventSignatures()
+
+        // Add errors for every manifest event signature that is not
+        // present in the ABI
+        return manifestEvents.reduce(
+          (errors, manifestEvent, index) =>
+            abiEvents.includes(manifestEvent)
+              ? errors
+              : errors.push(
+                  immutable.fromJS({
+                    path: [...path, index],
+                    message: `\
+Event with signature '${manifestEvent}' not present in ABI '${abi.name}'.
+Available events:
+${abiEvents
+                      .sort()
+                      .map(event => `- ${event}`)
+                      .join('\n')}`,
+                  })
+                ),
+          errors
+        )
+      }, immutable.List())
+  }
+
+  static validateTransactionFunctions(manifest, { resolveFile }) {
+    return manifest
+      .get('dataSources')
+      .filter(dataSource => dataSource.get('kind') === 'ethereum/contract')
+      .reduce((errors, dataSource, dataSourceIndex) => {
+        let path = ['dataSources', dataSourceIndex, 'transactionHandlers']
+
+        let abi
+        try {
+          // Resolve the source ABI name into a real ABI object
+          let abiName = dataSource.getIn(['source', 'abi'])
+          let abiEntry = dataSource
+            .getIn(['mapping', 'abis'])
+            .find(abi => abi.get('name') === abiName)
+          abi = ABI.load(abiEntry.get('name'), resolveFile(abiEntry.get('file')))
+        } catch (e) {
+          // Ignore errors silently; we can't really say anything about
+          // the transaction functions if the ABI can't even be loaded
+          return errors
+        }
+
+        // Obtain event signatures from the mapping
+        let manifestFunctions = dataSource
+          .getIn(['mapping', 'transactionHandlers'], immutable.List())
+          .map(handler => handler.get('function'))
+
+        // Obtain event signatures from the ABI
+        let abiFunctions = abi.transactionFunctionSignatures()
+
+        // Add errors for every manifest event signature that is not
+        // present in the ABI
+        return manifestFunctions.reduce(
+          (errors, manifestFunction, index) =>
+            abiFunctions.includes(manifestFunction)
+              ? errors
+              : errors.push(
+                  immutable.fromJS({
+                    path: [...path, index],
+                    message: `\
+Transaction function with signature '${manifestFunction}' not present in ABI '${
+                      abi.name
+                    }'.
+Available transaction functions:
+${abiFunctions
+                      .sort()
+                      .map(tx => `- ${tx}`)
+                      .join('\n')}`,
+                  })
+                ),
+          errors
+        )
       }, immutable.List())
   }
 
@@ -300,6 +355,7 @@ At least one such handler must be defined.`,
     let errors = immutable.List.of(
       ...Subgraph.validateAbis(manifest, { resolveFile }),
       ...Subgraph.validateContractAddresses(manifest),
+      ...Subgraph.validateEthereumContractHandlers(manifest),
       ...Subgraph.validateEvents(manifest, { resolveFile }),
     )
 
@@ -308,6 +364,7 @@ At least one such handler must be defined.`,
       ...Subgraph.validateRepository(manifest, { resolveFile }),
       ...Subgraph.validateDescription(manifest, { resolveFile }),
       ...Subgraph.validateEthereumContractHandlers(manifest)
+      ...Subgraph.validateTransactionFunctions(manifest, { resolveFile })
     )
 
     if (errors.size > 0) {
